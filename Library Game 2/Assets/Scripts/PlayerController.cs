3eef98cd7ea8837e9e0d055676749de4e419502f
Jlord423 <34775267+Jlord423@users.noreply.github.com>
2023-07-03 00:49:23 -0700

    Character Movement Added
    Script for Smooth movement in Iso added. https://www.youtube.com/watch?v=8ZxVBCvJDWk

--- Library Game 2/Assets/Scripts/PlayerController.cs
+++ Library Game 2/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnspeed = 360;
     private Vector3 _input;
+
+    void Update()
+    {
+        GatherInput();
+        Look();
+    }
 
     void GatherInput()
     {
-        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical));"));
+        // _input is a vector determined bt WASD with zero for the y axis
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
 
     void FixedUpdate()
@@ -18,8 +26,24 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
+    void Look()
+    {
+        if (_input != Vector3.zero)
+        {
+         
+
+            var relative = (transform.position + _input.ToIso()) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            // Ensures turning is a smooth process 
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnspeed * Time.deltaTime);
+        }
+     
+    }
     void Move()
     {
-        _rb.MovePosition(transform.position + transform.forward * _speed * Time.deltaTime);
+        // _input.magnitude gives the length of the vector,
+        // TODO character travels twice as fast in combined direction 
+        _rb.MovePosition(transform.position + (transform.forward * _input.normalized.magnitude) * _speed * Time.deltaTime);
     }
 }
