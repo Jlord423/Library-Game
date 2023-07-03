5dc448e15c44447eea5b7736d154c153115d6b91
Jlord423 <34775267+Jlord423@users.noreply.github.com>
2023-07-02 22:56:55 -0700

    Working on movement
    Basic scripting in progress

--- Library Game 2/Assets/Scripts/PlayerController.cs
+++ Library Game 2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    private Vector3 _input;
+
+    void GatherInput()
     {
-        
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical));"));
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        _rb.MovePosition(transform.position + transform.forward * _speed * Time.deltaTime);
     }
 }
