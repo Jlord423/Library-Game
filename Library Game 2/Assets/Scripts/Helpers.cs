426a4d3f479bc6b88430b9a759b9489727d6c260
Jlord423 <34775267+Jlord423@users.noreply.github.com>
2023-07-03 01:15:25 -0700

    Camera follow implement
    Camera now follows character movement

--- Library Game 2/Assets/Scripts/Helpers.cs
+++ Library Game 2/Assets/Scripts/Helpers.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public static class Helpers
 {
+
+
     private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
 
+    // changes input by 45 degrees to fix isometric movement 
     public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
 }
