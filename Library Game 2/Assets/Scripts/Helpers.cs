af151c0ca7bb6e656aa857d14ace4d8ae01f4cf6
WYJustin1213 <89850922+WYJustin1213@users.noreply.github.com>
2023-07-05 11:39:15 -0700

    Very buggy shooting

--- Library Game 2/Assets/Scripts/Helpers.cs
+++ Library Game 2/Assets/Scripts/Helpers.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Helpers
-{
-
-
-    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
-
-    // changes input by 45 degrees to fix isometric movement 
-    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
-}
+//public static class Helpers
+//
+//
+//    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+//
+//    // changes input by 45 degrees to fix isometric movement 
+//    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
+//}
